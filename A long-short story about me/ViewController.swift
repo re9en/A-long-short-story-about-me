b6f8996c76041fac69af29cbd9da7f4f177e9c19
re9en <regen69neger@gmail.com>
2021-04-05 22:26:21 +0300

    Initial Commit

---
+++ A long-short story about me/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A long-short story about me
+//
+//  Created by admin2 on 05.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
